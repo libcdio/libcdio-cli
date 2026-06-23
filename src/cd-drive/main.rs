32d10128c2eb1c94d4cb502a90aeed18cee6f873
Shiva Kiran Koninty <shiva@skran.xyz>
2026-06-23 14:18:56 +0530

    libcdio-cli/cd-drive: Use default drives if path not provided

--- src/cd-drive/main.rs
+++ src/cd-drive/main.rs
@@ -1,13 +1,41 @@
+use std::path::PathBuf;
+
+use anyhow::Result;
 use clap::Parser;
+use libcdio_rs::Drive;
 
 use crate::cli::Cli;
 
 mod cli;
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Cli::parse();
 
     libcdio_cli::setup_logs(cli.debug);
 
-    let _drive = cli.drive.positional.or(cli.drive.option);
+    let drive = cli.drive.positional.or(cli.drive.option);
+
+    if let Some(drive) = drive {
+        print_drive_info(drive)?;
+    } else if let drives = Drive::drives()
+        && !drives.is_empty()
+    {
+        for drive in drives {
+            if let Err(err) = print_drive_info(drive) {
+                println!("{err:?}");
+            }
+            println!();
+        }
+    } else {
+        println!("no drives connected");
+    }
+
+    Ok(())
+}
+
+fn print_drive_info(drive: PathBuf) -> Result<()> {
+    println!("Using drive {}", drive.display());
+    let _drive = Drive::with_drive(drive)?;
+
+    Ok(())
 }
